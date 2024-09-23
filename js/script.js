e60cabe677357ca12a048eb5f2952b8e3692cb47
mania605 <kim.minsun83@gmail.com>
2024-09-23 15:18:35 +0900

    data.js에 테마 관련 데이터 따로 저장한뒤, export, import로 데이터 안전하게 전달

--- js/script.js
+++ js/script.js
@@ -1,21 +1,11 @@
-/*
-  scope : 특정 변수의 정보값에 접근할 수 있는 범위 유효범위 
-  전역변수 : 시스템상에서 모두 접근할 수 있는 변수 (window객체에 등록되는 변수)
-  지역변수 : 특정 코드 블록안쪽에서 선언된 변수 : 선언된 코드블록 안쪽에서만 접근 가능
-
-  전역변수의 오염 : window객체에 등록된 변수는 다른 스크립트 파일이라고 하더라도 해당 변수값을 공유하므로
-  실수로 A.js에서 B.js에 있는 변수를 변형시킬수 있는 이슈
-
-  script연결시 type='module'이라고 지정하면 해당 파일 안쪽의 선언된 변수는 설사 코드블록 밖에서 선언되었더라도
-  파일자체의 전용 스코프가 설정되어 다른파일에서 해당 파일의 변수 접근이 불가능한 모듈화 상태 유지
-
-  type='module'지정시 defer와 마찬가지로 스크립트와 body안쪽코드가 비동기적으로(동시에) 해석됨
-*/
-
+//data.js에서 내보낸 themeData를 비구조화할당으로 불러옴
+//모듈타입의 스크립트 파일들에서는 export, import로 데이터값 전달
+import { themeData } from "./data.js";
 const main = document.querySelector("main");
 const numbers = document.querySelectorAll(".screen span");
 const [am, pm] = document.querySelectorAll(".screen em");
 
+//1초마다 반복 실행
 setInterval(() => {
   changeTheme();
   getTime().forEach((num, idx) => setTime(num, idx));
@@ -31,7 +21,6 @@ function getTime() {
   if (hr >= 13) {
     am.classList.remove("on");
     pm.classList.add("on");
-    //13시가 넘어설때 12를 뺀 값을 다시 hr변수에 재할당
     hr = hr - 12;
   } else {
     pm.classList.remove("on");
@@ -50,13 +39,6 @@ function setTime(num, index) {
 function changeTheme() {
   const hr = new Date().getHours();
 
-  const themeData = [
-    { className: "morning", period: [5, 11] },
-    { className: "afternoon", period: [11, 16] },
-    { className: "evening", period: [16, 20] },
-    { className: "night", period: [20, 5] }
-  ];
-
   main.className = "";
 
   themeData.forEach(data => {
@@ -67,4 +49,3 @@ function changeTheme() {
     }
   });
 }
-//2시 30분 까지 위의 반복문 패턴 정리
--- js/test.js
+++ js/test.js
@@ -1,1 +1,9 @@
-console.log(main);
+const themeData = [
+  { className: "morning", period: [5, 11] },
+  { className: "afternoon", period: [11, 16] },
+  { className: "evening", period: [16, 20] },
+  { className: "night", period: [20, 5] }
+];
+
+//해당 data.js안에 있는 데이터중 themeData라는 값만 비구조화할당으로 추출해서 내보냄
+export { themeData };
